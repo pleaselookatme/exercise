1499e054eff82e1fe880162d1d2afef95b962d0d
李小刚 <15005172190@163.com>
2014-07-17 16:37:18 +0800

    no message

---
+++ 7.17作业5.playground/section-1.swift
@@ -0,0 +1,15 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+var f = 0 , sum = 0
+for var i = 10;i >= 1;i-- {
+    if i == 10{
+        f = 1
+    } else {
+        f = (f + 1)*2
+    }
+    sum += f
+}
+println("\(sum)")
